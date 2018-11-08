cc3a55e5f014a1cc9655a57d0aeab2d25c90acc4
Roger Jungemann <roger@thefifthcircuit.com>
2018-11-08 09:57:35 -0800

    Use modern Dart syntax for async functions

--- lib/user_analytics.dart
+++ lib/user_analytics.dart
@@ -4,10 +4,9 @@ import 'package:intl/intl.dart';
 import 'package:http/http.dart' as http;
 
 class UserAnalytics {
-  const String listEndpoint = '/streams';
-  const String describeEndpoint = '/streams';
-  const String postEndpoint = '/streams';
-
+  String listEndpoint = '/streams';
+  String describeEndpoint = '/streams';
+  String postEndpoint = '/streams';
   String endpoint;
   String token;
   http.BaseClient client;
@@ -25,7 +24,7 @@ class UserAnalytics {
     this.client = new http.Client();
   }
 
-  Map<String, dynamic> list() async {
+  Future<Map<String, dynamic>> list() async {
     final http.Response response = await this.client.get(
       '${this.endpoint}${this.listEndpoint}',
       headers: this._headers,
@@ -34,7 +33,7 @@ class UserAnalytics {
     return json.decode(response.body);
   }
 
-  Map<String, dynamic> describe(String streamName) async {
+  Future<Map<String, dynamic>> describe(String streamName) async {
     final http.Response response = await this.client.get(
       '${this.endpoint}${this.describeEndpoint}/${streamName}',
       headers: this._headers,
@@ -43,7 +42,7 @@ class UserAnalytics {
     return json.decode(response.body);
   }
 
-  Map<String, dynamic> post(String streamName, Map<String, dynamic> data) async {
+  Future<Map<String, dynamic>> post(String streamName, Map<String, dynamic> data) async {
     final http.Response response = await this.client.post(
       '${this.endpoint}${this.postEndpoint}/${streamName}',
       headers: this._headers,
